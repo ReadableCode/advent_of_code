b23135021f64fe162f30d5a83bb56aada40de579
jason <jasonwchristiansen@gmail.com>
2024-12-21 14:58:09 -0600

    second half of day 4

--- 2024/day_04/day_04.py
+++ 2024/day_04/day_04.py
@@ -68,157 +68,159 @@ def get_text_input_list_of_lists(file_path):
     return ls_all_lines
 
 
-def get_left_to_right_string_at_coords(ls_ls_text, start_row, start_col, num_chars=4):
-    end_col = start_col + num_chars
+def get_text_from_ls_coords(ls_ls_text, ls_coords):
+    ls_text = []
 
-    # drop end_col if exceeds len
-    end_col = min(end_col, len(ls_ls_text[start_row]))
+    for coords in ls_coords:
+        ls_text.append(ls_ls_text[coords[0]][coords[1]])
 
-    ls_return_string = ls_ls_text[start_row][start_col:end_col]
-
-    print(f"ls_left_to_right_string: {ls_return_string}")
-
-    return ls_return_string
+    return ls_text
 
 
-def get_right_to_left_string_at_coords(ls_ls_text, start_row, start_col, num_chars=4):
-    # Calculate the end column as the inclusive lower bound
-    end_col = start_col - (num_chars)
+def get_ls_cords(ls_ls_text, start_row, start_col, direction, num_chars=4):
+    ls_coords = [[start_row, start_col]]
+    prev_coords = [start_row, start_col]
+    for char_idx in range(num_chars - 1):
+        if direction == "up_right":
+            new_coords = [prev_coords[0] - 1, prev_coords[1] + 1]
+        elif direction == "up_left":
+            new_coords = [prev_coords[0] - 1, prev_coords[1] - 1]
+        elif direction == "down_right":
+            new_coords = [prev_coords[0] + 1, prev_coords[1] + 1]
+        elif direction == "down_left":
+            new_coords = [prev_coords[0] + 1, prev_coords[1] - 1]
+        elif direction == "right":
+            new_coords = [prev_coords[0] - 0, prev_coords[1] + 1]
+        elif direction == "left":
+            new_coords = [prev_coords[0] - 0, prev_coords[1] - 1]
+        elif direction == "up":
+            new_coords = [prev_coords[0] - 1, prev_coords[1] + 0]
+        elif direction == "down":
+            new_coords = [prev_coords[0] + 1, prev_coords[1] + 0]
+        else:
+            return None
 
-    # If end_col is less than 0, use None to include the first element
-    if end_col < 0:
-        end_col = None
+        prev_coords = new_coords
 
-    # Slice from start_col to end_col in reverse
-    ls_return_string = ls_ls_text[start_row][start_col:end_col:-1]
+        # if out of bounds, skip
+        if new_coords[0] < 0 or new_coords[0] > ((len(ls_ls_text) - 1)):
+            continue
+        if new_coords[1] < 0 or new_coords[1] > (len(ls_ls_text[0]) - 1):
+            continue
 
-    print(f"ls_right_to_left_string: {ls_return_string}")
+        ls_coords.append(new_coords)
 
-    return ls_return_string
-
-
-ls_ls_text = [
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-    ["M", "M", "S", "X", "M", "A", "S", "M", "M", "M"],
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-]
-start_row = 1
-start_col = 8
-
-ls_return_string = get_right_to_left_string_at_coords(
-    ls_ls_text, start_row, start_col, num_chars=4
-)
-
-print(ls_return_string)
-assert ls_return_string == ["M", "M", "S", "A"]
-
-start_row = 1
-start_col = 3
-
-ls_return_string = get_right_to_left_string_at_coords(
-    ls_ls_text, start_row, start_col, num_chars=4
-)
-
-print(ls_return_string)
-assert ls_return_string == ["X", "S", "M", "M"]
-
-start_row = 1
-start_col = 2
-
-ls_return_string = get_right_to_left_string_at_coords(
-    ls_ls_text, start_row, start_col, num_chars=4
-)
-
-print(ls_return_string)
-assert ls_return_string == ["S", "M", "M"]
+    return ls_coords
 
 
-# %%
+def get_all_matches_xmas(ls_ls_text):
+    ls_directions = [
+        "up_right",
+        "up_left",
+        "down_right",
+        "down_left",
+        "right",
+        "left",
+        "up",
+        "down",
+    ]
+    search_text = "XMAS"
+    ls_search_text = list(search_text)
 
-
-def find_occurances_at_coords(
-    ls_ls_text, start_row, start_col, search_text, num_chars=4
-):
-    search_text = list(search_text)
-
-    occruances_found = 0
-
-    ls_left_to_right_string = get_left_to_right_string_at_coords(
-        ls_ls_text, start_row, start_col, num_chars=num_chars
-    )
-    if ls_left_to_right_string == search_text:
-        occruances_found += 1
-
-    ls_right_to_left_string = get_right_to_left_string_at_coords(
-        ls_ls_text, start_row, start_col, num_chars=num_chars
-    )
-    if ls_right_to_left_string == search_text:
-        occruances_found += 1
-
-    # find top to bottom occurance
-    ls_top_to_bottom_string = []
-    for parse_row in range(start_row, start_row + num_chars):
-        ls_top_to_bottom_string.append(ls_ls_text[parse_row][start_col])
-    print(f"ls_top_to_bottom_string: {ls_top_to_bottom_string}")
-    if ls_top_to_bottom_string == search_text:
-        occruances_found += 1
-
-    # find top to bottom occurance
-    ls_bottom_to_top_string = []
-    for parse_row in range(start_row, start_row + num_chars):
-        ls_bottom_to_top_string.append(ls_ls_text[parse_row][start_col])
-    ls_bottom_to_top_string = ls_bottom_to_top_string[::-1]
-    print(f"ls_bottom_to_top_string: {ls_bottom_to_top_string}")
-    if ls_bottom_to_top_string == search_text:
-        occruances_found += 1
-
-    return occruances_found
-
-
-ls_ls_text = [
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-    ["M", "M", "S", "X", "M", "A", "S", "M", "M", "M"],
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-    ["M", "M", "S", "A", "M", "X", "S", "M", "M", "M"],
-]
-start_row = 1
-start_col = 3
-search_text = "XMAS"
-
-occruances_found = find_occurances_at_coords(
-    ls_ls_text, start_row, start_col, search_text
-)
-
-print(occruances_found)
-
-
-# %%
-
-
-def find_horizonatal_occurances(line_list, both_directions=True):
-    print(f"Searching for occruances in line:\n{line_list}")
-    ls_search_strings = [list("XMAS")]
-    if both_directions:
-        ls_search_strings.append(list("SAMX"))
-
-    print(f"Searching for: {ls_search_strings}")
     count_occurances = 0
-    for i in range(len(line_list)):
-        if line_list[i : i + 4] in ls_search_strings:
-            print(f"Found {line_list[i:i+4]} at position {i}")
-            count_occurances += 1
+    ls_coords_matches = []
+    for start_row in range(len(ls_ls_text)):
+        for start_col in range(len(ls_ls_text[0])):
+            for direction in ls_directions:
+                ls_coords = get_ls_cords(
+                    ls_ls_text, start_row, start_col, direction=direction, num_chars=4
+                )
 
-    return count_occurances
+                ls_text = get_text_from_ls_coords(ls_ls_text, ls_coords)
+                is_match = ls_text == ls_search_text
+                if is_match:
+                    count_occurances += 1
+                    coord_match_data = {"ls_coords": ls_coords, "direction": direction}
+                    ls_coords_matches.append(coord_match_data)
+
+    return count_occurances, ls_coords_matches
 
 
-ls_all_lines = get_text_input_list_of_lists("day_04_input.txt")
-pprint_ls(ls_all_lines)
-for line_list in ls_all_lines:
-    count_occurances = find_horizonatal_occurances(line_list, both_directions=True)
-    break
+ls_ls_text = get_text_input_list_of_lists("day_04_input.txt")
+pprint_ls(ls_ls_text)
+
+count_occurances, ls_coords_matches = get_all_matches_xmas(ls_ls_text)
+print(f"There are {count_occurances} of XMAS in all directions")
+
+
+# %%
+
+
+def get_all_matches_mas_diag(ls_ls_text):
+    ls_directions = [
+        "up_right",
+        "up_left",
+        "down_right",
+        "down_left",
+    ]
+    search_text = "MAS"
+    ls_search_text = list(search_text)
+
+    count_occurances = 0
+    ls_coords_matches = []
+    for start_row in range(len(ls_ls_text)):
+        for start_col in range(len(ls_ls_text[0])):
+            for direction in ls_directions:
+                ls_coords = get_ls_cords(
+                    ls_ls_text, start_row, start_col, direction=direction, num_chars=3
+                )
+
+                ls_text = get_text_from_ls_coords(ls_ls_text, ls_coords)
+                is_match = ls_text == ls_search_text
+                if is_match:
+                    count_occurances += 1
+                    coord_match_data = {"ls_coords": ls_coords, "direction": direction}
+                    ls_coords_matches.append(coord_match_data)
+
+    return count_occurances, ls_coords_matches
+
+
+ls_ls_text = get_text_input_list_of_lists("day_04_input.txt")
+pprint_ls(ls_ls_text)
+
+count_occurances, ls_coords_matches = get_all_matches_mas_diag(ls_ls_text)
+print(f"There are {count_occurances} of MAS in diag directions")
+pprint_ls(ls_coords_matches)
+
+# %%
+
+
+def find_intersections_of_coords(ls_coords_matches):
+    ls_mid_coords = []
+    for dict_data in ls_coords_matches:
+        mid_coords = dict_data["ls_coords"][1]
+        ls_mid_coords.append(mid_coords)
+
+    ls_mid_coords = list((tuple(item) for item in ls_mid_coords))
+    print(f"ls_mid_coords: {ls_mid_coords}")
+    ls_set_mid_coords = list(set(ls_mid_coords))
+    print(f"ls_set_mid_coords: {ls_set_mid_coords}")
+
+    count_intersections = 0
+    ls_intersections = []
+    for unique_mid_coords in ls_set_mid_coords:
+        if ls_mid_coords.count(unique_mid_coords) > 1:
+            count_intersections += 1
+            ls_intersections.append(unique_mid_coords)
+
+    return count_intersections, ls_intersections
+
+
+pprint_ls(ls_coords_matches)
+count_intersections, ls_intersections = find_intersections_of_coords(ls_coords_matches)
+
+pprint_ls(ls_intersections, ls_title="Ls Intersections")
+print(count_intersections)
 
 
 # %%
