3fe5d259c97151147267c7aa8e5b2408be3f43b6
jason <jasonwchristiansen@gmail.com>
2025-12-01 16:53:54 -0600

    solve part 2 of day 1

--- 2025/day_01/day_01_part_01.py
+++ 2025/day_01/day_01_part_01.py
@@ -41,15 +41,15 @@ def move_dial(starting_location, direction, number_moves, debug=False):
         if direction == "L":
             if curr_location == 0:
                 curr_location = len(dial_locations) - 1
-                times_passed_zero += 1
             else:
                 curr_location -= 1
         elif direction == "R":
             if curr_location == len(dial_locations) - 1:
                 curr_location = 0
-                times_passed_zero += 1
             else:
                 curr_location += 1
+        if curr_location == 0:
+            times_passed_zero += 1
         if debug:
             print(f"    curr_location: {curr_location}")
 
@@ -125,92 +125,29 @@ print(
 )
 assert times_zero_hit == 969
 
+# Part 02 Example #
+# Be careful: if the dial were pointing at 50, a single rotation like R1000 would cause the dial
+# to point at 0 ten times before returning back to 50!
+ending_loc, times_passed_zero = move_dial(50, "R", 1000, debug=True)
+print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
+assert times_passed_zero == 10
+
+# Part 02 Special Case #
+ending_loc, times_zero_passed = move_dial(0, "L", 1, debug=True)
+print(f"ending_loc: {ending_loc}, times_zero_passed: {times_zero_passed}")
+assert times_zero_passed == 0  # this will FAIL with your current move_dial
+
 # Part 02 #
-# ls_input = get_text_input_lists("day_01_part_01_input.txt")
-# starting_location = 50
-# debug = False
-# ending_location, times_zero_hit, times_zero_passed = perform_moves(
-#     starting_location, ls_input, debug=debug
-# )
-# print(
-#     f"Ending location: {ending_location}, times_zero_hit: {times_zero_hit}, times_zero_passed: {times_zero_passed}"
-# )
-# # assert times_zero_passed ==
-
-
-# %%
-# Tests Ongoing #
-
-
-ls_input = ["R100"]
-starting_location = 0
-debug = True
+ls_input = get_text_input_lists("day_01_part_01_input.txt")
+starting_location = 50
+debug = False
 ending_location, times_zero_hit, times_zero_passed = perform_moves(
     starting_location, ls_input, debug=debug
 )
 print(
     f"Ending location: {ending_location}, times_zero_hit: {times_zero_hit}, times_zero_passed: {times_zero_passed}"
 )
-assert times_zero_passed == 1
-
-
-# %%
-
-
-ending_loc, times_passed_zero = move_dial(10, "R", 112, debug=False)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(90, "R", 10, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(0, "L", 1, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(50, "L", 68, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(82, "L", 30, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(52, "R", 48, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(2, "L", 2, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-# Be careful: if the dial were pointing at 50, a single rotation like R1000 would cause the dial
-# to point at 0 ten times before returning back to 50!
-ending_loc, times_passed_zero = move_dial(50, "R", 1000, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(0, "R", 1000, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(52, "R", 48, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
-
-# %%
-
-ending_loc, times_passed_zero = move_dial(0, "R", 100, debug=True)
-print(f"ending_loc: {ending_loc}, times_passed_zero: {times_passed_zero}")
+assert times_zero_passed == 5887
 
 
 # %%
