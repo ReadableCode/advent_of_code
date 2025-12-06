cae72d9a5bee7b87e4c0697785ee53688e61d06c
jason <jasonwchristiansen@gmail.com>
2025-12-06 09:24:44 -0600

    init 2

--- 2025/day_06/day_06_part_01.py
+++ 2025/day_06/day_06_part_01.py
@@ -18,17 +18,66 @@ def get_text_input_lines(file_path, debug=False):
             print(text_line)
         print("------------------------------------")
 
-    return text_lines
+    ls_cleaned_row_lists = []
+    for row in text_lines:
+        if debug:
+            print(row)
+
+        ls_cleaned_row_lists.append(row.split())
+
+    return ls_cleaned_row_lists
 
 
-
-def get_problems(file_path):
+def get_problems(file_path, debug=False):
     ls_problems = []
-    
-    ls_rows = get_text_input_lines(file_path)
-    
-    for row in ls_rows:
-        
+
+    ls_cleaned_row_lists = get_text_input_lines(file_path)
+
+    for row in ls_cleaned_row_lists:
+        if debug:
+            print(row)
+
+    for col_num in range(0, len(ls_cleaned_row_lists[0])):
+        if debug:
+            print(f"col_num: {col_num}")
+        parts_this_problem = []
+        for row_num in range(0, len(ls_cleaned_row_lists)):
+            if debug:
+                print(f"row_num: {row_num}")
+            parts_this_problem.append(ls_cleaned_row_lists[row_num][col_num])
+
+        ls_problems.append(parts_this_problem)
+
+    return ls_problems
+
+
+def solve_problem(problem, debug=False):
+    symbol = problem[-1]
+
+    terms = problem[:-1]
+
+    if debug:
+        print(f"symbol: {symbol}")
+        print(f"terms: {terms}")
+
+    if symbol == "*":
+        total = 1
+        for term in terms:
+            total = total * term
+    elif symbol == "+":
+        total = 0
+        for term in terms:
+            total = total + term
+
+    return total
+
+
+file_path = "day_06_part_01_input_test.txt"
+ls_problems = get_problems(file_path, debug=False)
+for problem in ls_problems:
+    print(problem)
+    total = solve_problem(problem, debug=True)
+    print(total)
 
 
 # %%
