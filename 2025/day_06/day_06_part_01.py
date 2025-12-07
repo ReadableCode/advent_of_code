4ff1b1cd72867795dc665001c46c73076c771231
jason <jasonwchristiansen@gmail.com>
2025-12-06 20:22:37 -0600

    finish part 1

--- 2025/day_06/day_06_part_01.py
+++ 2025/day_06/day_06_part_01.py
@@ -63,21 +63,65 @@ def solve_problem(problem, debug=False):
     if symbol == "*":
         total = 1
         for term in terms:
-            total = total * term
+            total = total * int(term)
     elif symbol == "+":
         total = 0
         for term in terms:
-            total = total + term
+            total = total + int(term)
+    else:
+        raise ValueError(f"unknown symbol: {symbol}")
 
     return total
 
 
+problem = ["123", "45", "6", "*"]
+answer = solve_problem(problem)
+assert answer == 33210
+
+problem = ["328", "64", "98", "+"]
+answer = solve_problem(problem)
+assert answer == 490
+
+problem = ["51", "387", "215", "*"]
+answer = solve_problem(problem)
+assert answer == 4243455
+
+problem = ["64", "23", "314", "+"]
+answer = solve_problem(problem)
+assert answer == 401
+
+
+# %%
+
+
+def solve_ls_problems(ls_problems, debug=False):
+    total_answer = 0
+    for problem in ls_problems:
+        if debug:
+            print(f"---------- problem: {problem} ----------")
+
+        total = solve_problem(problem)
+
+        if debug:
+            print(f"total: {total}")
+
+        total_answer = total_answer + total
+
+    return total_answer
+
+
 file_path = "day_06_part_01_input_test.txt"
 ls_problems = get_problems(file_path, debug=False)
-for problem in ls_problems:
-    print(problem)
-    total = solve_problem(problem, debug=True)
-    print(total)
+total_answer = solve_ls_problems(ls_problems, debug=True)
+print(f"total_answer: {total_answer}")
+assert total_answer == 4277556
+
+
+file_path = "day_06_part_01_input.txt"
+ls_problems = get_problems(file_path, debug=False)
+total_answer = solve_ls_problems(ls_problems, debug=True)
+print(f"total_answer: {total_answer}")
+assert total_answer == 4277556  # part 01 answer
 
 
 # %%
